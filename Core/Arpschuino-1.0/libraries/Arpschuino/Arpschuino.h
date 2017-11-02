b7fb8cb03ccfbb1d778b28870689de6fa999af5b
arpschuino <33263552+arpschuino@users.noreply.github.com>
2017-11-02 19:04:46 +0100

    Add files via upload

--- Core/Arpschuino-1.0/libraries/Arpschuino/Arpschuino.h
+++ Core/Arpschuino-1.0/libraries/Arpschuino/Arpschuino.h
@@ -2,4 +2,10 @@
 #ifndef Arpschuino_h
 #define Arpschuino_h
 
+#define default_adress (1)//adresse par defaut
+
+void led_temoin ();//change l'etat (allume ou eteind) de la led verte de l'arpschuino
+
+void arpdress_board();//prend l'adresse depuis l'arpdress board, l'ecrit dans l'EEPROM
+
 #endif
