8ad4571da193cbb753b87ade14e671f28c637974
arpschuino <33263552+arpschuino@users.noreply.github.com>
2019-01-12 12:47:37 +0100

    modification pour compatibilité avec 0.0.5

--- Core/Arpschuino/libraries/arpPCA9685/examples/arpschuino-2spider-board/arpschuino-2spider-board.ino
+++ Core/Arpschuino/libraries/arpPCA9685/examples/arpschuino-2spider-board/arpschuino-2spider-board.ino
@@ -19,20 +19,20 @@ gradation pwm 12 bit, 1526Hz sur 16 circuits
 int nbre_circuits (32);
 int adress = default_adress;
 
-// called this way, it uses the default address 0x40
-Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x40);
-Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x41);
+Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x40);//dip 000000
+Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x41);//dip 000001
 
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+//////////////////////////////////
+unsigned long previousMillis = 0;
+const long interval = 500; 
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  arpdress_board();//prise en charge de l'arpdress board
+  Arp_arpdress_board();//prise en charge de l'arpdress board
   //à commenter pour une adresse fixe
   pwm1.begin();
   pwm2.begin();
-  pwm1.setPWMFreq(1600);  // This is the maximum PWM frequency
+  pwm1.setPWMFreq(1600);
   pwm2.setPWMFreq(1600);  // This is the maximum PWM frequency
     
   // save I2C bitrate
@@ -40,19 +40,22 @@ void setup() {
   // must be changed after calling Wire.begin() (inside pwm.begin())
   TWBR = 12; // upgrade to 400KHz!
 
-  bitWrite (PORTD,4,1);// equivalent a digitalWrite(led_temoin,HIGH);
+  digitalWrite(LED_BUILTIN,HIGH);
 
   ArduinoDmx0.attachRXInterrupt  (frame_received);
-  ArduinoDmx0.set_control_pin(7);    // Arduino output pin for MAX485 input/output control (connect to MAX485 pins 2-3) 
-  ArduinoDmx0.set_rx_address(adress);      // dmx start address
+  ArduinoDmx0.set_control_pin(ArpDMXControl);    // Arduino output pin for MAX485 input/output control (connect to MAX485 pins 2-3) 
+  ArduinoDmx0.set_rx_address(address);      // dmx start address
   ArduinoDmx0.set_rx_channels(nbre_circuits);     // number of rx channels
   ArduinoDmx0.init_rx(DMX512);        // starts universe 0 as rx
 }
 
 void loop() {
-  
-  //delay(500);//la led s'eteind apres 500ms sans reception DMX
-  //bitWrite (PORTD,4,1);
+
+ if(millis() - previousMillis >= interval) {
+    // une demi-seconde sans reception
+    previousMillis = millis();   
+    digitalWrite(LED_BUILTIN,  HIGH); // on éteind la led     
+ }
  for(int i=0;i<16;i++)
  {
     //pwm.setPWM(i, 0,ArduinoDmx0.RxBuffer[i]<<4);//courbe droite
@@ -63,5 +66,6 @@ void loop() {
 
 void frame_received(uint8_t universe) // cette boucle est executé à chaque réception d'une trame DMX
 {
-  led_temoin ();//la led clignote si elle reçoit un signal DMX  
+  Arp_led_temoin ();//la led clignote si elle reçoit un signal DMX
+  previousMillis = millis();  
 }
