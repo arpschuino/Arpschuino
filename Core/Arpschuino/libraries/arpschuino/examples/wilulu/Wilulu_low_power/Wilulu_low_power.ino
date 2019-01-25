d0de9d5410ad4ad0efd3078dc2f637a987974398
arpschuino <33263552+arpschuino@users.noreply.github.com>
2019-01-25 16:26:17 +0100

    1.0.6
    Afin de preserver les accus li-ion, la wilulu se met en veille si la tension d'alimentation descend sous 2.9v
    calcul_freq 	remplacé par:	 rf12_calcul_freq

--- Core/Arpschuino/libraries/arpschuino/examples/wilulu/Wilulu_low_power/Wilulu_low_power.ino
+++ Core/Arpschuino/libraries/arpschuino/examples/wilulu/Wilulu_low_power/Wilulu_low_power.ino
@@ -10,7 +10,9 @@ Low power 05/03/18
 Désactivation des pullups,de l'ADC, analog comparator et the brown-out detector
 la wilulu s'endort au bout de 1 sec sans réception
 puis check la reception pendant 1 sec toute les 4 sec
-clignotement de la led verte a la frequence d'une sec quand l'alim descend en dessous de 3.1V (low batt = battement de coeur)
+clignotement de la led verte a la frequence d'une sec quand l'alim descend en dessous de 3.5V (low batt = battement de coeur)
+
+20/01/19 : Afin de preserver les accus li-ion, la wilulu se met en veille si la tension d'alimentation descend sous 2.8v
 
 official 01/12/17
 Written for the wilulu  www.arpschuino.fr/wilulu_e.php
@@ -23,94 +25,84 @@ Low power 05/03/18
 turn off pullups, ADC, analog comparator and brown-out detector
 if 1 seconds without reception: the wilulu falls asleep for 4 sec
 then check the recetion again
-blinking of the green led at one sec  frequency when the power supply drops below 3.1V (low batt = heartbeat)
-*/
-  
+blinking of the green led at one sec  frequency when the power supply drops below 3.5V (low batt = heartbeat)
 
+20/01/19: In order to preserve the li-ion batteries, the wilulu goes to sleep mode if the supply voltage drops below 2.8v
+*/
 
 #include <Arpschuino.h>
 #include <ArpRFLib.h>
 #include <avr/sleep.h>
-byte NODEID = 4;  //Adresse RF 
+#define NODEID  4  //Adresse RF 
 #define NETWORKID  212  //adresse du reseau commune à toute les machine
 uint8_t band = RF12_868MHZ; //frequence de l'emeteur RF12
 //RF12_915MHZ America  // RF12_433MHZ
-
 float frequency_setting = 868.00; //beetween 863,00 > 870,00
 //float frequency_setting = 915.00 ;//902;1–918 MHz (America, China and eastern Pacific Islands)
 
-const byte WiluluAdress[4]={WiluluAdress0,WiluluAdress1,WiluluAdress2,WiluluAdress3};//
+const byte dip[4]={WiluluAdress0,WiluluAdress1,WiluluAdress2,WiluluAdress3};//
 char identifiant=0;
-
 //////////////////////////////////
 unsigned long previousMillis = 0;
 unsigned long previousMillisLowBatt = 0;
 const long interval = 1000; 
-
+uint8_t battIndex = 0;
 
 void setup() {
 
     pinMode(LED_BUILTIN,OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);//led allumée en l'abscence de signal
 
-
-
-    for (int i =0;i<4;i++)
-    {
-      pinMode(WiluluAdress[i],INPUT);
-      digitalWrite(WiluluAdress[i],HIGH);
-    }
     pinMode (WiluluOUT , OUTPUT);
 
     delay (100);
-    rf12_initialize(NODEID, band, NETWORKID,calcul_freq(frequency_setting));
+    rf12_initialize(NODEID, band, NETWORKID,rf12_calcul_freq(frequency_setting));
 
     for(int i=0;i<4;i++)
     {
-       if(!digitalRead(WiluluAdress[i]))
-       {
-        identifiant += 1<<(i);
-       }
+      pinMode(dip[i],INPUT);  //
+      digitalWrite(dip[i],HIGH);    // pullup
+      if(!digitalRead(dip[i]))  identifiant += 1<<(i);
+      digitalWrite(dip[i],LOW);  //desactivation des pullup
     }
   //a commenter pour les 16 premiere adresses :    
   //identifiant+=16; //plus 16
-
-//low power :
-    for (int i =0;i<4;i++)
-    {
-      digitalWrite(WiluluAdress[i],LOW);  //desactivation des pullup
-    }
   
-  ADCSRA &= ~(1<<ADEN);                     //turn off ADC
-  ACSR |= _BV(ACD);                         //disable the analog comparator
-  MCUCR |= _BV(BODS) | _BV(BODSE);          //turn off the brown-out detector  
+    ADCSRA &= ~(1<<ADEN);                     //turn off ADC
+    ACSR |= _BV(ACD);                         //disable the analog comparator
+    MCUCR |= _BV(BODS) | _BV(BODSE);          //turn off the brown-out detector  
+
+    rf12_control(0xC04A);//detection low-batt 3.2v//https://tools.jeelabs.org/rfm12b.html
 }
 
-void sleep4s() {
-  cli();
-  rf12_sleep(127);                      // wakeup in about 4s
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // do a complete power-down
-  sleep_enable();                      // enable sleep mode
-  sei();                               // allow interrupts to end sleep mode                                    
-  sleep_cpu();                         // go to sleep
-  sleep_disable();                     // disable sleep mode for safety
-  analogWrite (WiluluOUT ,0);          //extinction de la led "out"
-}
 
 void loop() { 
-    if (rf12_recvDone() && rf12_crc == 0) {
-      if (rf12_lowbat()){               //Return true if the supply voltage is below 3.1V
-        if(millis() - previousMillisLowBatt >= 2000) {
-          //led_temoin ();    //la led clignote a la frequence d'une seconde
-          digitalWrite(LED_BUILTIN,HIGH);
-          delay(30);
-          digitalWrite(LED_BUILTIN,LOW);
+   
+    if (rf12_recvDone() && rf12_crc == 0) 
+    {
+      switch (battIndex)
+      {
+        case 0:    
+          Arp_led_temoin ();      //la led clignote a chaque reception d'une trame
+          battIndex += rf12_lowbat();
+          if (battIndex)rf12_control(0xC043);//detection low-batt 2.6v//https://tools.jeelabs.org/rfm12b.html
+          break;
           
-          previousMillisLowBatt = millis();
-        }
-      }
-      else{
-        led_temoin ();      //la led clignote a chaque reception d'une trame
+        case 1:    
+          if(millis() - previousMillisLowBatt >= 2000) {
+            digitalWrite(LED_BUILTIN,HIGH);
+            delay(30);
+            digitalWrite(LED_BUILTIN,LOW);            
+            previousMillisLowBatt = millis();
+            battIndex += rf12_lowbat();
+          }
+          break;
+          
+        case 2:   
+          while (rf12_lowbat()) sleep4s();
+          rf12_sleep(-1);//reveille le rf12 
+          battIndex=1;
+          break;        
       }
       analogWrite (WiluluOUT , rf12_data[identifiant]);
       previousMillis = millis();
@@ -119,8 +111,8 @@ void loop() {
    {
       unsigned long currentMillis = millis();
      
-      if(currentMillis - previousMillis >= 3000) {
-        // une seconde sans reception    
+      if(currentMillis - previousMillis >= 10000) {
+        // 10 secondes sans reception    
         digitalWrite(LED_BUILTIN,  LOW);
         sleep4s();
        // sleep4s(); //4 sec de plus
@@ -129,4 +121,16 @@ void loop() {
         previousMillis = millis();     
        }
    }
+}
+
+void sleep4s() {
+  digitalWrite(LED_BUILTIN, LOW);      //extinction de la led verte
+  analogWrite (WiluluOUT ,0);          //extinction de la led "out"
+  cli();
+  rf12_sleep(127);                     // wakeup in about 4s
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // do a complete power-down
+  sleep_enable();                      // enable sleep mode
+  sei();                               // allow interrupts to end sleep mode                                    
+  sleep_cpu();                         // go to sleep
+  sleep_disable();                     // disable sleep mode for safety 
 }
--- Core/Arpschuino/libraries/arpschuino/examples/wilulu/Wilulu_on_off/Wilulu_on_off.ino
+++ Core/Arpschuino/libraries/arpschuino/examples/wilulu/Wilulu_on_off/Wilulu_on_off.ino
@@ -5,6 +5,7 @@ on/off sur un canal
 fonctionne avec "emeteur_arpschuino_officiel" ou "emeteur_JLink_DMX" comme emmeteur
 adresse = adreesse de l'emeteur + valeur définie par l'encodeur rotatif
 En l'absence de signal, la led reste allumé, elle clignote lorsqu'elle recoit
+20/01/19 : Afin de preserver les accus li-ion, la wilulu se met en veille si la tension d'alimentation descend sous 2.8v
 
 official 01/12/17
 Written for the wilulu  www.arpschuino.fr/wilulu_e.php
@@ -12,11 +13,13 @@ on/off one channel
 work with "emeteur_arpschuino_officiel" or "emeteur_JLink_bridge_arp" as emiter
 address = emiter address + value set by the rotary encoder
 If there is no signal, the LED remains lit, flashes when it is received
-
+20/01/19: In order to preserve the li-ion batteries, the wilulu goes to sleep mode if the supply voltage drops below 2.8v
 */
+
 #include <Arpschuino.h>
 #include <ArpRFLib.h>
-byte NODEID = 4;  //Adresse RF
+#include <avr/sleep.h>
+uint8_t NODEID = 4;  //Adresse RF
 #define NETWORKID  212  //adresse du reseau commune à toute les machine
 uint8_t band = RF12_868MHZ; //frequence de l'emeteur RF12
 //RF12_915MHZ America  // RF12_433MHZ
@@ -34,56 +37,79 @@ unsigned long previousMillis = 0;
 const long interval = 1000; 
 
 
-
 void setup() {
 
     pinMode(LED_BUILTIN,OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);//led allumée en l'abscence de signal
 
-    for (int i =0;i<4;i++)
-    {
-      pinMode(dip[i],INPUT);
-      digitalWrite(dip[i],HIGH);
-    }
-
     pinMode (WiluluOUT , OUTPUT);
 
     delay (100);
-    rf12_initialize(NODEID, band, NETWORKID,calcul_freq(frequency_setting));
+    rf12_initialize(NODEID, band, NETWORKID,rf12_calcul_freq(frequency_setting));
 
-  for(int i=0;i<4;i++)
-  {
-     if(!digitalRead(dip[i]))
-     {
-      identifiant += 1<<(i);
-     }
-  }
+    for(int i=0;i<4;i++)
+    {
+      pinMode(dip[i],INPUT);  //
+      digitalWrite(dip[i],HIGH);    // pullup
+      if(!digitalRead(dip[i]))  identifiant += 1<<(i);
+      digitalWrite(dip[i],LOW);  //desactivation des pullup
+    }
   //a commenter pour les 16 premiere adresses :    
   //identifiant+=16; //plus 16
+
+  //////////////////////////////energy saver/////////////////////////
+  ADCSRA &= ~(1<<ADEN);                     //turn off ADC
+  ACSR |= _BV(ACD);                         //disable the analog comparator
+  MCUCR |= _BV(BODS) | _BV(BODSE);          //turn off the brown-out detector      
+
+  rf12_control(0xC043);//detection low-batt//https://tools.jeelabs.org/rfm12b.html
+  //C040:2.2V;C041:2.3V;..........C043:2.5V;............C04F:3.7V;
+  
+  //après passage dans la diode et le régulateur, la tension chute de 0.3v environ.
+  //donc si la detection est réglé a 2.5v le wilulu se met en veille a environ 2.8v
+
+   // after passing through the diode and regulator, the voltage drops about 0.3v.
+   // so if the detection is set to 2.5v the wilulu goes to sleep at about 2.8v  
 }
 
 void loop() {
+      
+    if (rf12_recvDone() && rf12_crc == 0) {
+      Arp_led_temoin ();
+      if(rf12_data[identifiant]>bascule)
+      {
+        digitalWrite (WiluluOUT , HIGH);
+      }
+      else
+      {
+        digitalWrite (WiluluOUT , LOW);
+      }
+   }
+   else
+   {
+      unsigned long currentMillis = millis();
+     
+      if(currentMillis - previousMillis >= interval) {
+        // une seconde sans reception
+        previousMillis = currentMillis;   
+        digitalWrite(LED_BUILTIN,  HIGH);      
+       }
+   }
+   if (rf12_lowbat())
+   {
+      while (rf12_lowbat()) sleep4s();
+      rf12_sleep(-1);//reveille le rf12 
+   }
+}
 
-      if (rf12_recvDone() && rf12_crc == 0) {
-        led_temoin ();
-        if(rf12_data[identifiant]>bascule)
-        {
-          digitalWrite (WiluluOUT , HIGH);
-        }
-        else
-         {
-          digitalWrite (WiluluOUT , LOW);
-        }
-     }
-     else
-     {
-        unsigned long currentMillis = millis();
-       
-        if(currentMillis - previousMillis >= interval) {
-          // une seconde sans reception
-          previousMillis = currentMillis;   
-
-          digitalWrite(LED_BUILTIN,  HIGH);      
-         }
-     }
+void sleep4s() {
+  digitalWrite(LED_BUILTIN, LOW);      //extinction de la led verte
+  analogWrite (WiluluOUT ,0);          //extinction de la led "out"
+  cli();                               //disable interrupt
+  rf12_sleep(127);                     // wakeup in about 4s
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // do a complete power-down
+  sleep_enable();                      // enable sleep mode
+  sei();                               // allow interrupts to end sleep mode                                    
+  sleep_cpu();                         // go to sleep
+  sleep_disable();                     // disable sleep mode for safety 
 }
